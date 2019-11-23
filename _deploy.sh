b5409f5fabadf1bec35bf8139b0c9994811f0e07
Isaiah Nyabuto <isaiahnyabuto@gmail.com>
2019-11-23 11:15:44 +0300

    update

--- _deploy.sh
+++ _deploy.sh
@@ -8,7 +8,7 @@ set -e
 git config --global user.email "isaiahnyabuto@gmail.com"
 git config --global user.name "Isaiah Nyabuto"
 
-git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+git clone -b master https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git im-data-hub
 cd im-data-hub
 cp -r ../_book/* ./
 git add --all *
